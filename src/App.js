2d6f0c624209a3502b0887c25dca47aeea60abfd
andrianazaliznak <andrianazaliznyak@gmail.com>
2023-02-01 23:31:19 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Simpson from "./components/Sipmpson/Simpson";
 
 
 function App() {
   return (
 <div>
-
+<Simpson name={'Homer'} surname={'Simpson'} image={'https://en.wikipedia.org/wiki/File:Homer_Simpson_2006.png'}/>
 </div>
   );
 }
---
+++ src/components/Sipmpson/Simpson.js
@@ -0,0 +1,13 @@
+
+const Simpson = (props) => {
+    const {name,surname,image}=props
+    return (
+        <div>
+            <div>name: {name}</div>
+            <div>surname: {surname}</div>
+            <img src={image} alt={name}/>
+        </div>
+    );
+};
+
+export default Simpson;
