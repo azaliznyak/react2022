6a401db4140a7bcfcc34fc52250563caefa038ed
andrianazaliznak <andrianazaliznyak@gmail.com>
2023-02-01 23:35:50 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Simpson from "./components/Sipmpson/Simpson";
 function App() {
   return (
 <div>
-<Simpson name={'Homer'} surname={'Simpson'} image={'https://en.wikipedia.org/wiki/File:Homer_Simpson_2006.png'}/>
+<Simpson name={'Homer'} surname={'Simpson'} image={'http://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'}/>
+  <Simpson nm/>
 </div>
   );
 }
